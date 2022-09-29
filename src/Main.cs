30f02c0aa066f19ba0d2fd125ff9c155b62b4618
Riddick <arsenijpavlov@gmail.com>
2022-09-29 18:31:42 +0300

    Add classes, but not work it

---
+++ src/Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
+
+namespace transportexpansion
+{
+    class Main : ModSystem
+    {
+        public override bool ShouldLoad(EnumAppSide forSide)
+        {
+            return true;
+        }
+
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+
+            api.RegisterEntity("EntityAnimalBot", typeof(EntityAnimalBot));
+        }
+    }
+
+    class FollowingBehaviour : EntityBehavior
+    {
+        public FollowingBehaviour(Entity entity) : base(entity)
+        {
+        }
+
+        public override string PropertyName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
